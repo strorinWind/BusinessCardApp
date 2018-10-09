125e9a6e6e145bb5169f32b65f0e3e4bb8d87339
strorinWind <sakunchik1@yandex.ru>
2018-10-09 21:29:26 +0300

    2 columns in landscape mode

--- app/src/main/java/ru/strorin/businesscardapp/NewListActivity.java
+++ app/src/main/java/ru/strorin/businesscardapp/NewListActivity.java
@@ -1,13 +1,20 @@
 package ru.strorin.businesscardapp;
 
 import android.content.Intent;
+import android.content.res.Configuration;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import com.dgreenhalgh.android.simpleitemdecoration.grid.GridDividerItemDecoration;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import ru.strorin.businesscardapp.data.DataUtils;
@@ -28,8 +35,29 @@ public class NewListActivity extends AppCompatActivity {
         RecyclerView recyclerView = findViewById(R.id.recycler_view);
         recyclerView.setAdapter(new NewsItemRecyclerAdapter(this,
                 DataUtils.generateNews(), clickListener));
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
+        setRecyclerViewDecoration(recyclerView);
+    }
+
+    private void setRecyclerViewDecoration(RecyclerView recyclerView){
+        int orientation = getResources().getConfiguration().orientation;
+
+        if (orientation == Configuration.ORIENTATION_PORTRAIT) {
+            LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+            recyclerView.setLayoutManager(layoutManager);
+
+            DividerItemDecoration itemDecoration = new DividerItemDecoration(recyclerView.getContext(), layoutManager.getOrientation());
+            itemDecoration.setDrawable(ContextCompat.getDrawable(this, R.drawable.divider));
+            recyclerView.addItemDecoration(itemDecoration);
+        }
+        else {
+            int numColumns = 2;
+            recyclerView.setLayoutManager(new GridLayoutManager(this, numColumns));
+
+            Drawable divider = ContextCompat.getDrawable(this, R.drawable.divider);
+            GridDividerItemDecoration itemDecoration = new GridDividerItemDecoration(divider, divider, numColumns);
+            recyclerView.addItemDecoration(itemDecoration);
+        }
     }
 
     @Override
